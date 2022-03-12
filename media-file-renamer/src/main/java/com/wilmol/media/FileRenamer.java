cce402c15fbe3aab571a40617450313e74bbdfcc
Will Molloy <willjoemolloy@gmail.com>
2022-03-12 20:40:20 +1300

    Refactor, rename variables

--- media-file-renamer/src/main/java/com/wilmol/media/FileRenamer.java
+++ media-file-renamer/src/main/java/com/wilmol/media/FileRenamer.java
@@ -1,5 +1,6 @@
 package com.wilmol.media;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.base.Strings;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,51 +19,54 @@ class FileRenamer {
   private static final Logger log = LogManager.getLogger();
 
   void run(Path directory, boolean dryRun) throws IOException {
-    log.info("run(directory={}, dryRun={})", directory, dryRun);
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    log.info("run(directory={}, dryRun={}) started", directory, dryRun);
 
-    List<Path> paths = Files.list(directory).toList();
+    List<Path> seasons = Files.list(directory).toList();
 
-    log.info("Detected {} season(s)", paths.size());
+    log.info("Detected {} season(s)", seasons.size());
 
-    int season = 1;
-    for (Path path : paths) {
-      log.info("Processing Season {}", season);
-      nameEpisodes(path, season, dryRun);
-      season++;
+    int i = 1;
+    for (Path seasonDir : seasons) {
+      log.info("Processing Season {}", i);
+      nameEpisodes(seasonDir, i, dryRun);
+      i++;
     }
+
+    log.info("run finished - elapsed: {}", stopwatch.elapsed());
   }
 
   private void nameEpisodes(Path seasonDirectory, int season, boolean dryRun) throws IOException {
-    // may need to rename the files
+    // may need to rename the files manually first
     // E.g. "Episode 10" comes before "Episode 9"
     // So name it like "Episode 09"
-    List<Path> paths = Files.list(seasonDirectory).sorted().toList();
+    List<Path> episodes = Files.list(seasonDirectory).sorted().toList();
 
-    log.info("Detected {} episode(s)", paths.size());
+    log.info("Detected {} episode(s)", episodes.size());
 
-    int episode = 1;
-    for (Path path : paths) {
+    int i = 1;
+    for (Path episode : episodes) {
       String s = Strings.padStart(String.valueOf(season), 2, '0');
-      String e = Strings.padStart(String.valueOf(episode), 2, '0');
+      String e = Strings.padStart(String.valueOf(i), 2, '0');
 
-      String name = path.getFileName().toString();
+      String name = episode.getFileName().toString();
       String suffix = name.substring(name.lastIndexOf("."));
 
       String newName = "Episode S%sE%s%s".formatted(s, e, suffix);
-      Path newPath = path.resolveSibling(newName);
+      Path newPath = episode.resolveSibling(newName);
 
-      log.info("Renaming ({}/{}): {} -> {}", episode, paths.size(), path, newPath);
+      log.info("Renaming ({}/{}): {} -> {}", i, episodes.size(), episode, newPath);
       if (!dryRun) {
-        Files.move(path, newPath);
+        Files.move(episode, newPath);
       }
-      episode++;
+      i++;
     }
   }
 
   public static void main(String[] args) {
     try {
       FileRenamer app = new FileRenamer();
-      app.run(Path.of(""), false);
+      app.run(Path.of(""), true);
     } catch (Throwable e) {
       log.fatal("Fatal error", e);
     }
